2f2983431ac91b4fbee1d7fb2fefcc3873109373
francorebosio <76429460+francorebosio@users.noreply.github.com>
2020-12-23 08:00:31 -0500

    Subiendo Modulo 2

---
+++ Modulo2/src/funciones/potencia.py
@@ -0,0 +1,13 @@
+
+
+def potenciacion(num1, p):
+    """
+    Retorna la potencia de un num1 elevado a la p
+    """
+    return num1 ** p
+
+
+if __name__ == "__main__":
+    p = potenciacion(2,5)
+    
+    print(f'el valor de 2 a la 5 es {p}')
